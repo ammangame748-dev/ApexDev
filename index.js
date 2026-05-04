2dda99b19760d794015a85548c4d45026ce37b80
ammangame748-dev <ammangame748@gmail.com>
2026-05-04 04:44:43 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
-document.getElementById('orderForm').addEventListener('submit', function(e) {
-    e.preventDefault();
+const firebaseConfig = {
+  apiKey: "AIzaSyDmVHTi4xD8ScPYqm5PQ_o4Gmti9dWiIsQ",
+  authDomain: "://firebaseapp.com",
+  projectId: "apexdev-abdd9",
+  storageBucket: "apexdev-abdd9.firebasestorage.app",
+  messagingSenderId: "46524918249",
+  appId: "1:46524918249:web:b3fd61fcddec3b41b20378",
+  databaseURL: "https://firebaseio.com"
+};
 
-    const order = {
-        name: document.getElementById('clientName').value,
-        service: document.getElementById('serviceType').value,
-        details: document.getElementById('details').value,
-        date: new Date().toLocaleString()
-    };
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
 
-    // جلب الطلبات القديمة أو إنشاء مصفوفة جديدة
-    let orders = JSON.parse(localStorage.getItem('apexOrders')) || [];
-    orders.push(order);
+if(document.getElementById('orderForm')) {
+    document.getElementById('orderForm').addEventListener('submit', function(e) {
+        e.preventDefault();
+        const btn = document.getElementById('submitBtn');
+        btn.innerText = "جاري الإرسال...";
+        btn.disabled = true;
 
-    // حفظ الطلبات
-    localStorage.setItem('apexOrders', JSON.stringify(orders));
+        const name = document.getElementById('clientName').value;
+        const service = document.getElementById('serviceType').value;
+        const details = document.getElementById('details').value;
 
-    alert('تم إرسال طلبك بنجاح! سيتم التواصل معك قريباً.');
-    this.reset();
-});
+        database.ref('orders').push({
+            name: name, service: service, details: details, date: new Date().toLocaleString()
+        }).then(() => {
+            alert('تم إرسال طلبك بنجاح! سيتم التواصل معك قريباً 🔥');
+            document.getElementById('orderForm').reset();
+            btn.innerText = "إرسال الطلب للنظام";
+            btn.disabled = false;
+        });
+    });
+}
